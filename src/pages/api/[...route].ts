67961b150f90799a3173cb5a638a6dec95d12f1b
rohanop <141114696+rohanly@users.noreply.github.com>
2024-06-15 14:16:20 +0530

    initial commit

---
+++ src/pages/api/[...route].ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+import { handle } from "@hono/node-server/vercel";
+import type { PageConfig } from "next";
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello from Hono!",
+  });
+});
+
+export default handle(app);
